1e33035a248544b6789347947ceeabcdf8b48c9e
VARUN M <94423797+vishnumur777@users.noreply.github.com>
2023-08-14 12:37:07 +0000

    created vector databases

---
+++ ingest.py
@@ -0,0 +1,22 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+
+DATA_PATH="."
+DB_FAISS_PATH="vectorstores/db_faiss"
+
+def create_vector_db():
+    loader = DirectoryLoader(DATA_PATH,glob="11th_physics.pdf", loader_cls=PyPDFLoader)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+    model_kwargs = {'device':'cpu'})
+
+    db = FAISS.from_documents(texts,embeddings)
+    db.save_local(DB_FAISS_PATH)
+
+if __name__ == "__main__":
+    create_vector_db()
